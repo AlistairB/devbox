d752dd08a29393297f11d6bb454f4e7c1b7888f6
Alistair Burrowes <alistair.burrowes@gmail.com>
2018-08-08 21:05:42 +1000

    run.sh improvements

--- run.sh
+++ run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -Eexuo pipefail
 
-# generate github_public key
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/github_public -C <&0
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/github_public -C 'alistair@email.com'
 
 cat ~/.ssh/github_public.pub
 
 read -p "Add ssh key to github public and press enter"
 
-mkdir -p ~/util/devbox
+mkdir -p ~/util
+
+cd ~/util
+
+sudo apt update && sudo apt install -y git
 
 git clone git@github.com:AlistairB/devbox.git
 
-cd ~/util/devbox
+sudo apt remove -y git
+
+cd devbox
 
 auto/setup
